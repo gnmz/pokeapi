f64ba21fac2702b28f52c7b971cb3a3a90771f2d
gnmx <gnum1x@yandex.ru>
2020-11-26 16:36:48 +0300

    add route and getPokemonList

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
+import MainPage from './pages/MainPage/MainPage';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <h1>Pokemon</h1>
+        <Route path='/' exact component={MainPage} />
       </div>
     );
   }
--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -2,10 +2,26 @@ import React, { Component } from 'react';
 
 
 class MainPage extends Component {
+    state = {
+        pokemonList:[]
+    }
+    componentDidMount(){
+        this.getPokemonList();
+    }
+
+    getPokemonList = () => {
+        fetch('https://pokeapi.co/api/v2/pokemon/')
+        .then(res => res.json())
+        .then(data => this.setState({pokemonList: data.results}))
+    }
+
     render(){
+        const { pokemonList } = this.state;
         return(
             <div className='main-page'>
-
+                {pokemonList.map((item) => 
+                    <p key={item.name}>{item.name}</p>
+                )}
             </div>
         );
     }
